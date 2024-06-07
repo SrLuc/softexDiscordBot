9fd90fd32fee084e96279d2cebdc9a578999c24d
RUAN RICKELME LUCENA RAMOS <ruanlucenaramos@gmail.com>
2024-06-07 14:51:23 -0300

    adding commands and you own handler

--- bots/bot/bot1.ts
+++ bots/bot/bot1.ts
@@ -1,7 +1,36 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import fs from "fs";
+import path from "path";
+import { Client, Collection, GatewayIntentBits } from "discord.js";
 
-export const bot1 = new Client({ intents: [GatewayIntentBits.Guilds] });
+//bot1 client
+export const bot1 = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
-bot1.on("ready", () => {
-  console.log(`Bot 1 conectado como ${bot1.user?.tag}`);
+//commands collection for bot1
+bot1.commands = new Collection();
+
+//commands handler for bot1
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".ts"));
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const commands = require(filePath);
+  if ("data" in commands && "execute" in commands) {
+    bot1.commands.set(commands.data.name, commands);
+  } else {
+    console.log(`these commands on ${filePath} are not valid`);
+  }
+}
+console.log(bot1.commands);
+
+//bot1 ready event
+bot1.once("ready", () => {
+  console.log(`Bot 1 connected as ${bot1.user?.tag}`);
 });
--- index.ts
+++ index.ts
@@ -1,7 +1,3 @@
-import fs from "fs";
-import path from "path";
-import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
-
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -9,69 +5,8 @@ import { bot1 } from "./bots/bot/bot1";
 //import { bot2 } from "./bots/bot2";
 //import { bot3 } from "./bots/bot3";
 
-
+bot1.login(process.env.TOKEN1);
 //bot2.login(process.env.TOKEN2);
 //bot3.login(process.env.TOKEN3);
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    //GatewayIntentBits.GuildMessages,
-    //GatewayIntentBits.GuildMessageReactions,
-    //GatewayIntentBits.GuildMembers,
-    //GatewayIntentBits.GuildVoiceStates,
-    //GatewayIntentBits.GuildPresences,
-  ],
-});
 
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".ts"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
-    }
-  }
-}
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    }
-  }
-});
-
-
-
-bot1.login(process.env.TOKEN1);
